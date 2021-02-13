d7ff40a704fe2a4b217f1f10eb6fde39f4c64ee2
Megan <megan.c.yip@gmail.com>
2021-02-12 22:19:29 -0800

    Forecast weather function

--- src/weather.js
+++ src/weather.js
@@ -5,9 +5,9 @@ let hour = now.getHours();
 if (hour < 10) {
   hour = `0${hour}`;
 }
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
 }
 
 let days = [
@@ -39,50 +39,81 @@ let year = now.getFullYear();
 let date = now.getDate();
 
 let today = document.querySelector("#today-is");
-today.innerHTML = `Today is ${day}, ${month} ${date}, ${year} at ${hour}:${minute}`;
+today.innerHTML = `Today is ${day}, ${month} ${date}, ${year} at ${hour}:${minutes}`;
 
 getCoords();
 
-// City Info
+function formatHours(timestamp) {
+  let now = new Date(timestamp);
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+
+      return `${hour}:${minutes}`;
+    }
+  }
+}
+
+// Forecast Info
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  forecastElement.innerHTML = `
+            <div class="col-3">
+              <h5>
+                ${formatHours(forecast.dt)}
+                <br />
+                ${Math.round(forecast.main.temp)} °F
+                <br />
+                <img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    >
+              </h5>
+            </div>`;
 }
+//Weather Info
+function weatherInfo(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temp = Math.round(response.data.main.temp);
+  let realTemp = document.querySelector("#temperature");
+
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let conditionElement = document.querySelector("#condition");
+  let iconElement = document.querySelector("#icon");
+
+  fahrenheitResponse = response.data.main.temp;
+
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  conditionElement.innerHTML = `${response.data.weather[0].description}`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  realTemp.innerHTML = `${temp}`;
+}
+
+// City Info
 function city(event) {
   event.preventDefault();
-
   let cityName = document.querySelector(".city");
   let searchInput = document.querySelector("#city-search-input");
   cityName.innerHTML = `${searchInput.value}`;
-  //Weather Info
-  function weatherInfo(response) {
-    document.querySelector("#city").innerHTML = response.data.name;
-    let temp = Math.round(response.data.main.temp);
-    let realTemp = document.querySelector("#temperature");
-
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let conditionElement = document.querySelector("#condition");
-    let iconElement = document.querySelector("#icon");
-
-    fahrenheitResponse = response.data.main.temp;
-
-    humidityElement.innerHTML = `${response.data.main.humidity}%`;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    conditionElement.innerHTML = `${response.data.weather[0].description}`;
-    iconElement.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    realTemp.innerHTML = `${temp}`;
-  }
   let apiKey = "c87f4cfeec08e60e3fffae2d5c8fb202";
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(weatherInfo);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiUrl}&units=${units}`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
+
 let searchCity = document.querySelector("#search");
 searchCity.addEventListener("submit", city);
 
@@ -92,11 +123,13 @@ function getCoords() {
 }
 function thisPosition(position) {
   let apiKey2 = "c87f4cfeec08e60e3fffae2d5c8fb202";
+  let units = "imperial";
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let geoPositionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey2}&units=imperial`;
+  let geoPositionUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey2}&units=${units}`;
   axios.get(geoPositionUrl).then(currentPosition);
 }
+
 function currentPosition(response) {
   let currentTemperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#temperature");
@@ -105,6 +138,7 @@ function currentPosition(response) {
   let currentWind = document.querySelector("#wind");
   let currentCondition = document.querySelector("#condition");
   let currentIcon = document.querySelector("#icon");
+  let apiKey = "c87f4cfeec08e60e3fffae2d5c8fb202";
 
   fahrenheitResponse = response.data.main.temp;
 
@@ -117,6 +151,9 @@ function currentPosition(response) {
   );
   currentPlace.innerHTML = `${response.data.name}`;
   currentTemp.innerHTML = `${currentTemperature}`;
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${response.data.name}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let currentLocation = document.querySelector("#current-location");
@@ -133,6 +170,7 @@ function celciusConversion(event) {
   let celciusActual = document.querySelector("#temperature");
   celciusActual.innerHTML = Math.round(celciusTemp);
 }
+// Temp Conversion back to fahreneheit
 function fahrenheitConversion(event) {
   event.preventDefault();
   fahrenheit.classList.add("active");
